3660c01c732bfd747fe919a052b95c9b9c808882
Luca <lucamoro96@gmail.com>
2020-05-25 19:44:20 +0200

    Template

---
+++ Lab6/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
